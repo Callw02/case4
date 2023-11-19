3a03b1c4179e0215ddc6617226dd67c252a53649
Wille2002 <wilhelm.sjostedtthiel@gmail.com>
2023-11-19 15:06:31 +0100

    files are correct

--- JS/SignInOrUp.js
+++ JS/SignInOrUp.js
@@ -3,11 +3,11 @@
 //När man klickar på sign in så kommer man till sidan där man loggar in.
 let SignIn = document.getElementById("SignIn");
 SignIn.addEventListener("click", function (event) {
-    window.location.href = "./index.html";
+    window.location.href = "./login.html";
 })
 
 //När man klickar på sign up så kommer man till sidan där man registrerar sig. 
 let SignUp = document.getElementById("SignUp");
 SignUp.addEventListener("click", function (event) {
-    window.location.href = "./login.html";
+    window.location.href = "./register.html";
 })
